4394472bd7674c3e426263caa1b3343ea38517ec
Bruno Ribeiro <brunoribeiro.sch@gmail.com>
2024-02-13 22:10:05 -0300

    test: Cobertura da classe boot

--- src/test/java/io/github/brunorsch/sicredi/sessao/votacao/SicrediSessaoVotacaoApplicationTests.java
+++ src/test/java/io/github/brunorsch/sicredi/sessao/votacao/SicrediSessaoVotacaoApplicationTests.java
@@ -1,13 +1,15 @@
 package io.github.brunorsch.sicredi.sessao.votacao;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 class SicrediSessaoVotacaoApplicationTests {
 	@Test
 	void contextLoads() {
+		SicrediSessaoVotacaoApplication.main(new String[] {});
 	}
 }
